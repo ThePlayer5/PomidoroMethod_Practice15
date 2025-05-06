d1dd471896763cad97c192ce0d273c982eb5cf53
212301 <212301@1220-014-22>
2025-05-06 15:26:58 +0300

    Добавьте файлы проекта.

---
+++ PomidoroMethod/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PomidoroMethod
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<TodoItem> Tasks { get; set; } = new ObservableCollection<TodoItem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            TaskListLb.ItemsSource = Tasks;
+        }
+
+        private void AddTaskBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TaskInputTb.Text))
+            {
+                Tasks.Add(new TodoItem
+                {
+                    Title = TaskInputTb.Text,
+                    IsDone = false
+                });
+                TaskInputTb.Text = string.Empty;
+                UpdateCounter();
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Changed(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteTaskBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void UpdateCounter()
+        {
+            int counter = 0;
+            foreach (var task in Tasks)
+            {
+                if (!task.IsDone)
+                {
+                    counter++;
+                }
+            }
+            CounterTextTbl.Text = $"Осталось дел: {counter}";
+        }
+    }
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime? DueDate { get; set; }
+        public string Category { get; set; }
+    }
+    public class DoneToTextDecorationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isDone = (bool)value;
+            return isDone ? TextDecorations.Strikethrough : null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
